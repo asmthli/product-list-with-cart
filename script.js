2dc9731e44da8e892a3639f634af2d63580f83bb
Will <will.alan.smith@gmail.com>
2024-08-12 18:13:01 +0100

    Dynamically populate product images

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+const TABLET_BREAKPOINT = "768px";
+const DESKTOP_BREAKPOINT = "1024px";
+
 async function getProducts() {
     const response = await fetch("./data.json");
     const products = await response.json();
 
     return products;
+}
+
+function createPictureElement(imageSources) {
+    const mobileSrc = document.createElement("source");
+    mobileSrc.setAttribute("srcset", imageSources.mobile);
+
+    const tabletSrc = document.createElement("source");
+    tabletSrc.setAttribute("media", `(min-width: ${TABLET_BREAKPOINT})`);
+    tabletSrc.setAttribute("srcset", imageSources.tablet);
+
+    const desktopSrc = document.createElement("source");
+    desktopSrc.setAttribute("media", `(min-width: ${DESKTOP_BREAKPOINT})`);
+    desktopSrc.setAttribute("srcset", imageSources.desktop);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", imageSources.desktop);
+
+    const picElement = document.createElement("picture");
+    picElement.appendChild(desktopSrc);
+    picElement.appendChild(tabletSrc);
+    picElement.appendChild(mobileSrc);
+    picElement.appendChild(img);
+
+    return picElement;
 }
 
 function createListElement(product) {
@@ -18,10 +45,13 @@ function createListElement(product) {
     priceElement.textContent = product.price;
     priceElement.classList.add("product__PRICE");
 
+    const picElement = createPictureElement(product.image);
+
     const listElement = document.createElement("li");
     listElement.appendChild(nameElement);
     listElement.appendChild(categoryElement);
     listElement.appendChild(priceElement);
+    listElement.appendChild(picElement);
 
     return listElement;
 }
